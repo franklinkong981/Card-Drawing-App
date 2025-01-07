a59e1e8c4c5cdfaf67c6c4d77061c6ecf8abc950
Franklin Kong <franklinkong981@gmail.com>
2025-01-07 13:16:11 -0800

    Fixed error message condition, part 1 done

--- card-drawer/src/CardDrawer.jsx
+++ card-drawer/src/CardDrawer.jsx
@@ -10,23 +10,24 @@ const CardDrawer = () => {
   useEffect(() => {
     async function getNewDeck() {
       const res = await axios.get("https://deckofcardsapi.com/api/deck/new");
-      console.log(res);
-      setDeckId(res.deck_id);
+      setDeckId(res.data.deck_id);
     }
+    getNewDeck();
   }, []);
 
   //only calls the Deck of Cards API for a new card if there are still cards waiting to be drawn in the deck.
   const drawCard = async () => {
     if (anyCardsLeft) {
       const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw?count=1`);
-      console.log(res);
-      setCurrentCard(card => ({
-        imageUrl: res.cards[0].image,
-        value: res.cards[0].value,
-        suit: res.cards[0].suit
-      }));
-      if (res.remaining === 0) {
+      if (res.data.cards.length === 0) {
         setAnyCardsLeft(false);
+      }
+      else {
+        setCurrentCard(card => ({
+          imageUrl: res.data.cards[0].image,
+          value: res.data.cards[0].value,
+          suit: res.data.cards[0].suit
+        }));
       }
     }
   };
